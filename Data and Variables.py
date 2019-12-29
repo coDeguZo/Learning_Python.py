6f103c858cd9c935bd3e915c8739e6796264bdee
coDeguzo <u.ariguzo.ua@gmail.com>
2019-12-29 15:11:30 -0500

    Data and Variables.py and Functions.py

---
+++ Data and Variables.py
@@ -0,0 +1,12 @@
+character_name = "Uzoma"
+character_age = "25"
+is_Male = False
+print("There was once a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+
+character_name = "Chibu"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ". ")
+
+
---
+++ Functions.py
@@ -0,0 +1,16 @@
+#Functions are collection of code that performs a tasks
+#Core concpet of programming in Python
+
+#Saying Hi to a User
+'''Function term; can use _ or not; 
+All the code that comes after say hi line comes into play'''
+def say_hi(name, age): 
+    print("Hello " + name + ", you are " + age)
+
+print("Top")
+say_hi("Mike", "35")
+say_hi("Steve", "70")
+print("Bottom")
+
+'''When you have a grouping of code to perform a specific tasks, 
+thats when you can use functions.'''
