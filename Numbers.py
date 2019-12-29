d16b7fa7f7d1896d7d1d67530ea6085d38e351c5
coDeguzo <u.ariguzo.ua@gmail.com>
2019-12-29 13:59:13 -0500

    BuildingBasicCalculator.py

---
+++ Numbers.py
@@ -0,0 +1,57 @@
+print(2)
+print(2.0891)
+print(-2.0891)
+print( 3 + 4.5)
+print(3 * (5 + 4))
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+
+my_num = 5
+print(str(my_num)) 
+
+my_num = 5
+print(str(my_num) + " my favorite number")
+#Creates a string with an added string attachmnent
+
+my_num = 5
+print(abs(my_num))
+#Prints absolute number
+
+my_num = 5
+print(pow(3, 2)) 
+#3 to the power of 2
+
+my_num = 5
+print(max(3, 2)) 
+#Which number is highest
+
+my_num = 5
+print(min(3, 2)) 
+#Which number is smaller
+
+my_num = 5
+print(round(3.7)) 
+#Rounds number follows rounding rules
+
+from math import *
+#Imports the math module into my command line.  Intelliscence will be able to let me know what code I can use and what I can't.
+#Gives us math functions
+
+my_num = 5
+print(floor(3.5))
+#Rounds number down
+
+my_num = 5
+print(ceil(3.5))
+#Rounds number up
+
+my_num = 5
+print(sqrt(36))
+#Finds the square root of number.  36/2=6
+
+print(cos(36))
+
+
+#Math Operations and functions with the numbers. :)
